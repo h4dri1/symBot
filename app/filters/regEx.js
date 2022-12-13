8504b1f540410b41de682fb9b1f7f1abaf2b6862
h4dri1 <hadrien.vuillaume@gmail.com>
2022-12-13 21:51:35 +0100

    add: working with movies other files will not be linked need to add music

--- app/filters/regEx.js
+++ app/filters/regEx.js
@@ -5,7 +5,11 @@ const regExFilters = {
     episodeRegEx: /([Eex]([0-9]{2})(?:[^0-9]|$))/,
     yearRegEx: /[\[\(]?(19|20)\d{2}[\)\]]?/,
     nameRegEx: /(.+?)(?=[\[\(]?(19|20)\d{2}[\)\]]?|[Ss]?([0-9]{1,2}))/,
-    formatRegEx: /^.*\.(avi|mp4|mkv|flv|mov|wmv|mpg|mpeg|mp3|m4a|ogg|flac|wav|aiff|alac|zip|rar|7z|tar|gz|jpeg|jpg|png|gif|bmp|iso|img|bin|nfo|srt|exe|msi|dmg|app)$/
+    formatRegEx: /^.*\.(avi|mp4|mkv|flv|mov|wmv|mpg|mpeg|mp3|m4a|ogg|flac|wav|aiff|alac|nfo|srt|sub|vtt|ass|ssa|txt)$/,
+    formatMoviesRegEx: /^.*\.(avi|mp4|mkv|flv|mov|wmv|mpg|mpeg)$/,
+    formatMusicRegEx: /^.*\.(mp3|m4a|ogg|flac|wav|aiff|alac)$/,
+    formatSubRegEx: /^.*\.(srt|sub|idx|ass|ssa|txt)$/,
 }
 
 module.exports = regExFilters
+
--- index.js
+++ index.js
@@ -12,20 +12,26 @@ const { linkTorrent } = require('./app/linkers/link')
 
 // Extract torrent name
 // Use regex to extract season, episode and year
-const season = myArgs[0].match(regExFilters.saisonRegEx);
-const episode = myArgs[0].match(regExFilters.episodeRegEx);
-const year = myArgs[0].match(regExFilters.yearRegEx);
-const nameTorrent = myArgs[0].match(regExFilters.nameRegEx);
 const format = myArgs[0].match(regExFilters.formatRegEx);
 
-// Clean torrent name
-const torrentName =  clean({ 
-    name: nameTorrent ? nameTorrent[1] : myArgs[0], 
-    season: season ? season[1] : null,
-    episode: episode ? episode[1] : null, 
-    year: year ? year[0] : null,
-    format: format ? `.${format[1]}` : null
-})
+if (format[0].match(regExFilters.formatMoviesRegEx)) {
+    const season = myArgs[0].match(regExFilters.saisonRegEx);
+    const episode = myArgs[0].match(regExFilters.episodeRegEx);
+    const year = myArgs[0].match(regExFilters.yearRegEx);
+    const nameTorrent = myArgs[0].match(regExFilters.nameRegEx);
 
-// Create symbolic link
-linkTorrent(torrentName, myArgs[0]);
+    // Clean torrent name
+    const torrentName =  clean({ 
+        name: nameTorrent ? nameTorrent[1] : myArgs[0], 
+        season: season ? season[1] : null,
+        episode: episode ? episode[1] : null, 
+        year: year ? year[0] : null,
+        format: format ? `.${format[1]}` : null
+    })
+
+    // Create symbolic link
+    linkTorrent(torrentName, myArgs[0]);
+}
+
+
+
