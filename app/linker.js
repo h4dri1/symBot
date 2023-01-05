12c706336f898e08a2efaef5bc4b452db051f641
h4dri1 <hadrien.vuillaume@gmail.com>
2023-01-05 22:35:51 +0100

    add: end line to log file

--- app/linker.js
+++ app/linker.js
@@ -18,6 +18,7 @@ module.exports = {
                 // Create symbolic link
                 log('info: Start creating symbolic link...')
                 await symLink(myArgs, makeFolder(torrentName), 'TVShows', torrentName)
+                log('info: --------------------------------------------------------------- End of the process')
             }
             else {
                 // Movie
